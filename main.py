fb8f809d509d6809fd93b19e8c711e705eb2351a
Benjamin Van <bvanbever1@gmail.com>
2022-06-20 12:47:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# print([x for x in range(1, 11, 2) if x % 2 == 0])
+
+# print([x for x in range(10, 0, -1)])
+
+# Afficher la liste des multiples de 3 compris entre 10 inclus et 30 inclus
+
+print(sum([x for x in range (10, 31) if x % 3 == 0]))
